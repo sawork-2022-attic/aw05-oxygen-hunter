c61fdf891dc63e2e905ce2a3671beb665024142d
m1rr0r <xiao.li@smail.nju.edu.cn>
2022-04-21 11:37:50 +0800

    gateway finished

--- pos-gateway/src/main/java/com/micropos/gateway/GatewayApplication.java
+++ pos-gateway/src/main/java/com/micropos/gateway/GatewayApplication.java
@@ -11,7 +11,9 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+
 }
